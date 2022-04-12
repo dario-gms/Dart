69dc9e63db32902f0bdb14efc4ffefdf7e4bc3d8
Dário <86432208+dario-gms@users.noreply.github.com>
2022-04-12 14:09:58 -0300

    adicionado projeto inicial de dart

---
+++ projeto_inicial.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  int idade = 27;
+  double altura = 1.70;
+  bool geek = true;
+  String nome = 'Dário Gomes';
+  String apelido = 'Darinho';
+  String frase = 'Eu sou $apelido. \n'
+      'Mas meu nome completo é: $nome, \n'
+      'eu me considero geek? $geek. \n'
+      'Eu tenho $altura metros de altura e tenho \n'
+      '$idade anos de idade.';
+
+  print(frase);
+}
