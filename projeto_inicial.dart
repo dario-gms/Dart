2ec25d2181387c2a40dfa4f6e618cd3b3eb33b6e
Dário <86432208+dario-gms@users.noreply.github.com>
2022-04-14 13:47:51 -0300

    fase 2

--- projeto_inicial.dart
+++ projeto_inicial.dart
@@ -1,15 +1,22 @@
 void main() {
 
+
   int idade = 27;
   double altura = 1.70;
   bool geek = true;
   String nome = 'Dário Gomes';
   String apelido = 'Darinho';
-  String frase = 'Eu sou $apelido. \n'
-      'Mas meu nome completo é: $nome, \n'
-      'eu me considero geek? $geek. \n'
-      'Eu tenho $altura metros de altura e tenho \n'
-      '$idade anos de idade.';
+  List<dynamic> dario = [idade, altura, geek, nome, apelido];
+
+  String frase = 'Eu sou ${dario[4]}. \n'
+      'Mas meu nome completo é: ${dario[3]}, \n'
+      'eu me considero geek? ${dario[2]}. \n'
+      'Eu tenho ${dario[1]} metros de altura e tenho \n'
+      '${dario[0]} anos de idade.';
+
+  List<String> listanomes = ['Ricarth', 'Natália', 'Alex', 'Andriu', 'André'];
+
 
   print(frase);
+
 }
