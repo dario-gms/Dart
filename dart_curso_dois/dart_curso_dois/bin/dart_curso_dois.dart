fc4d74370a75da0bf7757db2b89ab4dfa06a2736
Dário <dario.gms@hotmail.com.br>
2022-04-19 15:51:21 -0300

    Update dart_curso_dois.dart

--- dart_curso_dois/dart_curso_dois/bin/dart_curso_dois.dart
+++ dart_curso_dois/dart_curso_dois/bin/dart_curso_dois.dart
@@ -1,3 +1,14 @@
 void main() {
-  print("Oi, mundo");
+  String nome = "Laranja";
+  double peso = 100.2;
+  String cor = "Verde e Amarela";
+  String sabor = "Doce e cítrica";
+  int diaDesdeColheita = 40;
+  bool isMadura;
+
+  if (diaDesdeColheita >= 30) {
+    isMadura = true;
+  } else {
+    isMadura = false;
+  }
 }
