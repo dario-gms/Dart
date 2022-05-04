6213990b7b78c5994f0f6b6227b82aa34a8d19f6
Dário <dario.gms@hotmail.com.br>
2022-05-04 15:08:34 -0300

    Update main.dart

--- dart_cursos_tres/alurabank/bin/main.dart
+++ dart_cursos_tres/alurabank/bin/main.dart
@@ -1,19 +1,13 @@
 void main() {
-  ContaCorrente contadaAmanda = ContaCorrente();
-  contadaAmanda.titular = "Amanda";
-  contadaAmanda.agencia = 123;
-  contadaAmanda.conta = 1;
-  contadaAmanda.saldo = 0.0;
+  ContaCorrente conta1 = ContaCorrente();
+  ContaCorrente conta2 = ContaCorrente();
 
-  ContaCorrente contadoTiago = ContaCorrente();
-  contadoTiago.titular = "Tiago";
-  contadoTiago.agencia = 123;
-  contadoTiago.conta = 2;
+  
 }
 
 class ContaCorrente {
   String titular;
-  int agencia;
+  int agencia = 145;
   int conta;
   double saldo = 0.0;
 }
