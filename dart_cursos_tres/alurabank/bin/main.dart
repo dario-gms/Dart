04ea1dea0bb9450c5c4536c3e4035ad7a4265f94
Dário <dario.gms@hotmail.com.br>
2022-05-05 20:06:31 -0300

    Update main.dart

--- dart_cursos_tres/alurabank/bin/main.dart
+++ dart_cursos_tres/alurabank/bin/main.dart
@@ -1,11 +1,15 @@
 void main() {
   ContaCorrente contaDaAmanda = ContaCorrente();
+  ContaCorrente contaDoTiago = ContaCorrente();
   contaDaAmanda.titular = "Amanda";
+  contaDoTiago.titular = "Tiago";
 
   contaDaAmanda.deposito(20.0);
   print("Saldo da ${contaDaAmanda.titular}: ${contaDaAmanda.saldo}");
   contaDaAmanda.saque(80.0);
   print("Saldo da ${contaDaAmanda.titular}: ${contaDaAmanda.saldo}");
+
+  bool sucesso = contaDaAmanda.transferencia(20.0, contaDoTiago);
 }
 
 class ContaCorrente {
@@ -14,7 +18,18 @@ class ContaCorrente {
   int conta;
   double saldo = 20.0;
 
-  void saque(double valorDoSaque) {
+  bool transferencia(double valorDeTransferencia, ContaCorrente contaDestino) {
+    if (this.saldo - valorDeTransferencia < -100) {
+      print("Sem saldo suficiente.");
+      return false;
+    } else {
+      this.saldo -= valorDeTransferencia;
+      contaDestino.deposito(valorDeTransferencia);
+      return true;
+    }
+  }
+
+  bool saque(double valorDoSaque) {
     if (this.saldo - valorDoSaque < -100) {
       print("Sem saldo suficiente.");
       return false;
