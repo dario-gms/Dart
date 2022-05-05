a7c28684bf5e4c468da21c5353bb3d934ccd7770
Dário <dario.gms@hotmail.com.br>
2022-05-05 14:45:00 -0300

    Update main.dart

--- dart_cursos_tres/alurabank/bin/main.dart
+++ dart_cursos_tres/alurabank/bin/main.dart
@@ -1,13 +1,24 @@
 void main() {
-  ContaCorrente conta1 = ContaCorrente();
-  ContaCorrente conta2 = ContaCorrente();
+  ContaCorrente contaDaAmanda = ContaCorrente();
+  contaDaAmanda.titular = "Amanda";
 
-  
+  print("Saldo da ${contaDaAmanda.titular}: ${contaDaAmanda.saldo}");
+  contaDaAmanda.saque(80.0);
+  print("Saldo da ${contaDaAmanda.titular}: ${contaDaAmanda.saldo}");
 }
 
 class ContaCorrente {
   String titular;
   int agencia = 145;
   int conta;
-  double saldo = 0.0;
+  double saldo = 20.0;
+
+  void saque(double valorDoSaque) {
+    if (this.saldo - valorDoSaque < -100) {
+      print("Sem saldo suficiente.");
+    } else {
+      print("Sacando $valorDoSaque reais");
+      this.saldo -= valorDoSaque;
+    }
+  }
 }
