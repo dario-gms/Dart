5616afb765d8a1b40bf458635a178908d4981565
Dário <dario.gms@hotmail.com.br>
2022-05-11 17:30:53 -0300

    Update main.dart

--- dart_cursos_tres/alurabank/bin/main.dart
+++ dart_cursos_tres/alurabank/bin/main.dart
@@ -15,6 +15,17 @@ void main() {
   print("Titular: ${contaDaAmanda.titular.nome}");
   print("CPF: ${contaDaAmanda.titular.cpf}");
   print("Profissão: ${contaDaAmanda.titular.profissao}");
+
+  Cliente tiago = Cliente()
+  ..nome = "Tiago"
+  ..cpf = "132.456.789-00"
+  ..profissao = "Programador Dart";
+
+  print("Titular: ${contaDoTiago.titular.nome}");
+  print("CPF: ${contaDoTiago.titular.cpf}");
+  print("Profissão: ${contaDoTiago.titular.profissao}");
+
+
 }
 
 
